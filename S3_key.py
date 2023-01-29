9f05728e8a83575cd15c6164a07ac7aeb870c673
LeDat98 <121386506+LeDat98@users.noreply.github.com>
2023-01-30 02:45:45 +0900

    Update S3_key.py

--- S3_key.py
+++ S3_key.py
@@ -1,3 +1,3 @@
 class S3_key:
-    aws_access_key_id = 'AKIAR56QI6RTALXWK3G3'
-    aws_secret_access_key = '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'
+    aws_access_key_id = ''
+    aws_secret_access_key = ''
